338d742e56336fc51976fc421af9ffdc17a9fd2c
sid-stack <55102866+sid-stack@users.noreply.github.com>
2020-11-02 10:12:39 +0530

    Update macroProcessor.java

--- macroProcessor.java
+++ macroProcessor.java
@@ -1,6 +1,6 @@
 /* MacroProcessor: MNT and MDT (Macros without param)
  * @author Sid-Stack
- * @version 1.1
+ * @version 1.1 
  */
 import java.util.*;
 import java.io.*;
@@ -9,25 +9,24 @@ public class macroProcessor
     static Set<String> mnt = new LinkedHashSet<>();
     static List<String> mdt = new ArrayList<>();
     static List<Integer> macro_ptr = new ArrayList<>();    
-    static int cnt=0;    
+    static int cnt=0;   
+    
     static void macroName(String name)
     {
         mnt.add(name);        
         cnt++;
     }
-
     static void macroData(String data)
     {
         mdt.add(data);        
     }
-
     static void printTable(String n)
     {
         int i = 0;
         if(n.equalsIgnoreCase("mnt"))
         {
             System.out.println("MNT:");
-            System.out.println("#\tName\tlc");
+            System.out.println("No.\tName\tLC");
             int k = 0;
             for(String item:mnt)
             {            
@@ -35,25 +34,27 @@ public class macroProcessor
                 System.out.println(i+"\t"+item+"\t"+macro_ptr.get(k));   //                
                 k++;
             }
+            System.out.println();
         }
-
         else if(n.equalsIgnoreCase("mdt"))
         {        
             System.out.println("MDT:");
-            System.out.println("#\tDATA");            
+            System.out.println("LC\tDATA");            
             for(String d:mdt)
             {
                 ++i;
                 System.out.println(i+"\t"+d);
             }
+            System.out.println();
         }
     }
 
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("File:");
+        System.out.println("File:"); 
         String fname = sc.nextLine();
+        System.out.println();
         try{
             File f = new File(fname);
             Scanner file = new Scanner(f);
